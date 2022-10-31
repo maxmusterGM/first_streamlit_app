1cd890f91d941e456b97edf2f8c7403452c23dfd
maxmusterGM <116805252+maxmusterGM@users.noreply.github.com>
2022-10-31 12:39:50 +0100

    Update streamlit_app.py
    added function to insert new line in snowflake
    added button to add fruit

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,7 +43,7 @@ try:
     
 except URLError as e:
   streamlit.error()
-
+  
 streamlit.header("The fruit load list contains:")
 # Snowflake-realted functions
 def get_fruit_load_list():
@@ -61,8 +61,15 @@ if streamlit.button('Get Fruit Load List'):
 streamlit.stop()
 
 # Allow end user to add a fruit to the list
+def insert_row_snowflake(new_fruit):
+  with my_cnx.cursor() as my_cur:
+    my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+    return "Thanks for adding " + new_fruit
+  
 add_my_fruit =  streamlit.text_input('What fruit would you like to add?')
-streamlit.write('Thanks for adding ', add_my_fruit)
+if streamlit.button('Add a Fruit to the List'):
+  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+  back_from_function = insert_row_snowflake(add_my_fruit)
+  streamlit.text(back_from_function)
 
-# This will not work currently, but just go with it for now
-my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+
